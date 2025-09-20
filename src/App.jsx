1a91541d7e4389f9bf34184e402c67027774bbed
VoloshynArtem <vartem.gg@gmail.com>
2025-09-20 18:06:12 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+import { useState, createContext, useContext } from 'react';
+import Print from "./Print.jsx";
+import './App.css';
+import './List.css'
+import './Input.css';
+
+
+const LabelItems = createContext();
+
+function App() {
+   const [items, setitems] = useState([]);
+  
+  return (
+    
+    <div className="app">
+      <LabelItems.Provider value={{ items, setitems }}>
+        
+        <div>
+          <Input />
+          <List />
+        </div>
+        
+
+        <div className="controlls">
+          <SubmitButton />
+          <PrintButton />
+
+        </div>
+
+        
+        
+
+      </LabelItems.Provider>
+
+
+    </div>
+  );
+}
+
+
+
+function SubmitButton(){
+
+  return <button type="submit" form="form">Submit</button>
+
+}
+
+function Input() {
+  const { items, setitems } = useContext(LabelItems);
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+
+    const value = e.target.elements.form.value;
+
+    console.log(value.length);
+
+    if(value.length >0){
+      setitems(previtems => [
+        {id: items.length,
+        text: value},
+        ...previtems
+      ]);
+      e.target.reset();
+    
+    }
+  
+  }
+
+  return (
+    <div>
+      <form id="form" onSubmit={handleSubmit}>
+        <input name="form" type="text" />
+
+      </form>
+    </div>
+  );
+  
+}
+
+function PrintButton(){
+  const {items} = useContext(LabelItems);
+  
+  function handleClick() {
+    Print(items);
+  }
+  
+  return(
+    <div>
+      <button onClick={handleClick}> print </button >
+    </div>
+
+  )
+}
+
+
+
+
+
+function List(){
+  const { items, setitems } = useContext(LabelItems);
+  
+  function deleteItem(id){
+    setitems(prevItems => prevItems.filter(item => item.id !== id));
+  
+  }
+  
+    return <div className="list">
+        {items.map(item => (
+          <button className="listButton" key={item.id} onClick={() => deleteItem(item.id)}>
+            {item.text}
+          </button>
+        ))}
+      </div>
+}
+export default App;
+
+
---
+++ src/Print.jsx
@@ -0,0 +1,56 @@
+
+export default function Print(items){
+  var iframe = document.createElement('iframe');
+  
+    
+  var html = `
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+    <script type="text/javascript" src="script.js"></script>
+
+    <style>
+      
+      @page {
+        size: auto;
+        margin: 0;
+      }
+
+      div.dotted {
+        border: 1px solid black;
+        
+        height: 16mm;
+        display: flex;
+        align-items: center; 
+        justify-content: center; 
+        box-sizing: border-box;
+        font-size: 16mm; 
+      }
+      
+      body{
+          display: flex;
+          margin: 4.2 mm;
+          top: 
+      }
+      
+    </style>
+  
+  </head>
+
+  <body>
+      ` 
+
+      for(const i of items.reverse()){
+         html += `<div class="dotted">` + i.text + `</div>`;
+       }
+      
+  html += '</html>'  
+;
+    document.body.appendChild(iframe);
+    iframe.style.display = 'none';
+    iframe.contentWindow.document.open();
+    iframe.contentWindow.document.write(html);
+    iframe.contentWindow.document.close();
+    iframe.contentWindow.print();
+}
